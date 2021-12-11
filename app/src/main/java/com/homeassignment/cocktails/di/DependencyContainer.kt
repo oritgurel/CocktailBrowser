96ddbcd9ca2e43b2edb926f7ab74f6fb532d36c1
Orit Malki <orit@hero-apps.io>
2021-12-11 22:10:47 +0200

    use gson type adapter instead of post-serialization mapper

--- app/src/main/java/com/homeassignment/cocktails/di/DependencyContainer.kt
+++ app/src/main/java/com/homeassignment/cocktails/di/DependencyContainer.kt
@@ -1,19 +1,23 @@
 package com.homeassignment.cocktails.di
 
+import com.google.gson.GsonBuilder
 import com.homeassignment.cocktails.BuildConfig.BASE_URL
 import com.homeassignment.cocktails.data.CocktailsRepository
 import com.homeassignment.cocktails.data.api.ApiService
-import com.homeassignment.cocktails.data.model.DrinkMapper
+import com.homeassignment.cocktails.data.model.Drink
+import com.homeassignment.cocktails.data.model.remote.DrinkTypeAdapter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object DependencyContainer {
 
+    private val gson = GsonBuilder()
+        .registerTypeAdapter(Drink::class.java, DrinkTypeAdapter())
+        .create()
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
     private val service = retrofit.create(ApiService::class.java)
-    private val drinkMapper = DrinkMapper()
-    val cocktailsRepo = CocktailsRepository(service, drinkMapper)
+    val cocktailsRepo = CocktailsRepository(service)
 }
